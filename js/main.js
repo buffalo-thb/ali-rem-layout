398dad292846454571e31086202fd4c1c6a700f7
buffalo-thb <justdoit_thb@163.com>
2017-07-12 11:25:47 +0800

    create new repo, main for ali-rem-layout

---
+++ js/main.js
@@ -0,0 +1,11 @@
+require.config({
+    shim: {
+        "jquery.dev" : {
+            exports: "$"
+        },
+    }
+    // "path" : {
+    //     "jquery" : ['https://code.jquery.com/jquery-3.2.1.min', 'libs/jquery'],
+    //     "huiwenshu" : ['js/huiwenshu']
+    // }
+});
